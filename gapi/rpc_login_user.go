41839198c0bcab16b22ba74d9dba891c593e13c7
phantom-sage <z4gamdev@gmail.com>
2026-08-10 14:32:21 +0200

    feat: add metadata extraction for user agent and client IP in LoginUser

--- gapi/rpc_login_user.go
+++ gapi/rpc_login_user.go
@@ -37,12 +37,13 @@ func (server *Server) LoginUser(ctx context.Context, req *pb.LoginUserRequest) (
 		return nil, status.Errorf(codes.Internal, "failed to create refresh token: %s", err)
 	}
 
+	mtdt := server.extractMetadata(ctx)
 	session, err := server.store.CreateSession(ctx, db.CreateSessionParams{
 		ID:           refreshTokenPayload.ID,
 		Username:     user.Username,
 		RefreshToken: refreshToken,
-		ClientIp:     "",
-		UserAgent:    "",
+		ClientIp:     mtdt.ClientIP,
+		UserAgent:    mtdt.UserAgent,
 		IsBlocked:    false,
 		ExpiresAt:    refreshTokenPayload.ExpiredAt,
 	})
